c733187a105c8899d0786010f3aa2d4794e7b3c4
LuckyDenis <belavin.d.andreevich@yandex.ru>
2020-09-04 20:08:21 +0300

    Add: Bellman-Ford algorithm. Rewrite: Dijkstra algorithm.

--- graph/dijkstra.py
+++ graph/dijkstra.py
@@ -10,30 +10,30 @@
 def djs(graph, start_vertex):
     costs = dict().fromkeys(graph.keys(), float("inf"))
     path = dict().fromkeys(graph.keys(), -1)
-    costs[start_vertex] = 0
 
-    queue = [[start_vertex, 0]]
+    costs[start_vertex] = 0
+    queue = [start_vertex]
+
     while queue:
-        v = queue.pop(0)[0]
+        v = queue.pop(0)
         for to, cost in g[v].items():
             if costs[v] + cost < costs[to]:
-                if [to, costs[to]] in queue:
-                    queue.remove([to, costs[to]])
-                path[to] = v
+                queue.append(to)
                 costs[to] = costs[v] + cost
-                queue.append([to, costs[to]])
+                path[to] = v
+
     return costs, path
 
 
 g = {
-    0: {2: 1, 3: 1},
-    1: {2: 4, 5: 6},
-    2: {0: 1, 1: 4, 4: 9, 5: 2, 6: 1},
-    3: {0: 1, 7: 4},
-    4: {2: 9},
-    5: {1: 6, 2: 2, 6: 3},
-    6: {2: 1, 5: 3, 7: 9},
-    7: {3: 4, 6: 9}
+    0: {4: 3},
+    1: {0: 1, 2: 3, 3: 5},
+    2: {0: 7, 1: 2},
+    3: {1: 4, 5: 5, 7: 8},
+    4: {2: 2, 6: 6},
+    5: {6: 9},
+    6: {7: 1},
+    7: {}
 }
 
 costs, path = djs(g, 0)
